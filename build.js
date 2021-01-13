44ba2cd68b5713f0899c487ce1bf8cd06915c9ee
silverwind <me@silverwind.io>
2021-01-13 23:42:03 +0100

    fix converter

--- build.js
+++ build.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 "use strict";
 
-const jsYaml = require("js-yaml");
-const fs = require("fs");
-const path = require("path");
+const {join} = require("path");
+const {load} = require("js-yaml");
+const {readFileSync, writeFileSync} = require("fs");
 
-const input = path.join(__dirname, ".eslintrc");
-const output = path.join(__dirname, "index.json");
+const input = join(__dirname, ".eslintrc");
+const output = join(__dirname, "index.json");
 
-const yaml = jsYaml.safeLoad(fs.readFileSync(input, "utf8"));
-fs.writeFileSync(output, JSON.stringify(yaml, null, 2));
+writeFileSync(output, JSON.stringify(load(readFileSync(input, "utf8")), null, 2));

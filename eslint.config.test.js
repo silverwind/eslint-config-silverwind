1ebf88bf8bab1a6e3e6771f6a0f451d79af35f6a
silverwind <me@silverwind.io>
2024-03-09 00:24:32 +0100

    separate export

--- eslint.config.test.js
+++ eslint.config.test.js
@@ -1,13 +1,18 @@
 import confs from "./eslint.config.js";
+import confs2 from "eslint-config-silverwind/flat"; // eslint-disable-line i/no-unresolved
 import eslintrc from "./index.json";
+import eslintrc2 from "eslint-config-silverwind";
 
 test("confs", () => {
-  expect(Array.isArray(confs)).toEqual(true);
-  for (const conf of confs) {
-    expect(Array.isArray(conf.files)).toEqual(true);
+  for (const c of [confs, confs2]) {
+    expect(Array.isArray(c)).toEqual(true);
+    for (const conf of c) {
+      expect(Array.isArray(conf.files)).toEqual(true);
+    }
   }
 });
 
 test("eslintrc", () => {
   expect(Array.isArray(eslintrc.plugins)).toEqual(true);
+  expect(Array.isArray(eslintrc2.plugins)).toEqual(true);
 });

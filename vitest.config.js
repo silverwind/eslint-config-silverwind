12bd296f2e052b6bbfa93bec3a68e9348c17d9c9
silverwind <me@silverwind.io>
2024-03-27 00:19:28 +0100

    update deps

--- vitest.config.js
+++ vitest.config.js
@@ -3,14 +3,9 @@ import {backendTest} from "vitest-config-silverwind";
 import {stringPlugin} from "vite-string-plugin";
 
 export default defineConfig({
-  test: {
-    ...backendTest({url: import.meta.url}),
-    exclude: [
-      "**/node_modules/**",
-      "**/dist/**",
-      "**/.{git,make}/**",
-    ],
-  },
+  test: backendTest({
+    url: import.meta.url,
+  }),
   plugins: [
     stringPlugin(),
   ],

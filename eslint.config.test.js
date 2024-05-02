dc354da4599db397cab3a28ecd314a08671b3f91
silverwind <me@silverwind.io>
2024-05-02 21:52:48 +0200

    remove self-dependency

--- eslint.config.test.js
+++ eslint.config.test.js
@@ -1,13 +1,10 @@
-import confs from "./eslint.config.js";
-import confs2 from "eslint-config-silverwind";
+import conf from "./eslint.config.js";
 import eslintrc from "./index.json";
 
 test("confs", () => {
-  for (const c of [confs, confs2]) {
-    expect(Array.isArray(c)).toEqual(true);
-    for (const conf of c) {
-      expect(Array.isArray(conf.files)).toEqual(true);
-    }
+  expect(Array.isArray(conf)).toEqual(true);
+  for (const c of conf) {
+    expect(Array.isArray(c.files)).toEqual(true);
   }
 });
 

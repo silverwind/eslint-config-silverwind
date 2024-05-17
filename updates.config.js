81ca1ce6054cf8cbc30ae35f5fd0b076a83c6d98
silverwind <me@silverwind.io>
2024-05-17 15:20:04 +0200

    pin typescript

--- updates.config.js
+++ updates.config.js
@@ -3,5 +3,6 @@ export default {
     "eslint", // needs flat config
     "eslint-plugin-array-func", // needs flat config
     "eslint-plugin-vitest", // needs flat config
+    "typescript", // https://github.com/silverwind/updates/issues/68
   ],
 };

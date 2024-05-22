5200765f0b722d78b5bbd1e66c839edc3f8fd190
silverwind <me@silverwind.io>
2024-05-22 20:52:35 +0200

    exclude eslint-plugin-no-use-extend-native

--- updates.config.js
+++ updates.config.js
@@ -3,6 +3,7 @@ export default {
     "eslint", // needs flat config
     "eslint-plugin-array-func", // needs flat config
     "eslint-plugin-vitest", // needs flat config
+    "eslint-plugin-no-use-extend-native", // needs flat config
     "typescript", // https://github.com/silverwind/updates/issues/68
   ],
 };

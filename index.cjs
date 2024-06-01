bc7a8adc8d048b7dde27bb5565ba7abe725a2259
silverwind <me@silverwind.io>
2024-06-01 20:13:37 +0200

    use tests dir

--- index.cjs
+++ index.cjs
@@ -121,7 +121,7 @@ module.exports = {
       },
     },
     {
-      "files": ["tests/e2e/**"],
+      "files": ["tests/**"],
       "extends": "plugin:playwright/recommended",
       "rules": {
         "playwright/expect-expect": [0],

9b3c80a1efed0ae354c119847db7a7e8367d4bde
silverwind <me@silverwind.io>
2024-08-23 14:55:33 +0200

    update ts

--- updates.config.js
+++ updates.config.js
@@ -4,6 +4,5 @@ export default {
     "eslint-plugin-array-func", // needs flat config
     "eslint-plugin-vitest", // needs flat config
     "eslint-plugin-no-use-extend-native", // needs flat config
-    "typescript", // https://github.com/silverwind/updates/issues/68
   ],
 };

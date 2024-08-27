04825a522580d4d89ee4f2cd44504ac4262ab1a3
silverwind <me@silverwind.io>
2024-08-27 02:47:08 +0200

    update deps, lock eslint-plugin-sonarjs

--- updates.config.js
+++ updates.config.js
@@ -4,5 +4,6 @@ export default {
     "eslint-plugin-array-func", // needs flat config
     "eslint-plugin-vitest", // needs flat config
     "eslint-plugin-no-use-extend-native", // needs flat config
+    "eslint-plugin-sonarjs", // TODO: review new rules from v2
   ],
 };

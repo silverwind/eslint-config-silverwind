8795223160ae0788c8a4ba828569392285753780
silverwind <me@silverwind.io>
2024-10-07 14:45:09 +0200

    downgrade @eslint/compat

--- updates.config.js
+++ updates.config.js
@@ -5,5 +5,6 @@ export default {
     "eslint-plugin-vitest", // needs flat config
     "eslint-plugin-no-use-extend-native", // needs flat config
     "eslint-plugin-sonarjs", // TODO: review new rules from v2
+    "@eslint/compat", // >= 1.2.0 needs eslint 9
   ],
 };

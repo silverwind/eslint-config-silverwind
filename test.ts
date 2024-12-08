85250ec7945afa7c60dd7eaa37bd09eab11bdf53
silverwind <me@silverwind.io>
2024-12-08 16:19:37 +0100

    upgrade eslint-plugin-sonarjs, add test.ts

---
+++ test.ts
@@ -0,0 +1,6 @@
+let a = 0;
+if (true) { // eslint-disable-line sonarjs/no-gratuitous-expressions,sonarjs/no-all-duplicated-branches
+  a = 1;
+} else {
+  a = 1; // eslint-disable-line @typescript-eslint/no-unused-vars
+}

8d956e1359fed92443bbc22fcae662926a0c415c
silverwind <me@silverwind.io>
2024-12-08 16:27:52 +0100

    tweaks

--- updates.config.js
+++ updates.config.js
@@ -1,8 +1,11 @@
 export default {
+  types: [
+    "dependencies",
+    "devDependencies",
+  ],
   exclude: [
     "eslint-plugin-array-func", // needs flat config
     // "eslint-plugin-vitest", // needs flat config
     "eslint-plugin-no-use-extend-native", // needs flat config
-    "eslint-plugin-sonarjs", // TODO: review new rules from v2
   ],
 };

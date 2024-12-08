bea8f851ac25107af795938be9322c1e2629c2df
silverwind <me@silverwind.io>
2024-12-08 15:40:32 +0100

    downgrade plugins

--- updates.config.js
+++ updates.config.js
@@ -1,3 +1,8 @@
 export default {
-  exclude: [],
+  exclude: [
+    "eslint-plugin-array-func", // needs flat config
+    // "eslint-plugin-vitest", // needs flat config
+    "eslint-plugin-no-use-extend-native", // needs flat config
+    "eslint-plugin-sonarjs", // TODO: review new rules from v2
+  ],
 };

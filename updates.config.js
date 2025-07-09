6c717d776868b2e0b48087eb22616071145b547b
silverwind <me@silverwind.io>
2025-07-09 15:01:17 +0200

    tweaks

--- updates.config.js
+++ updates.config.js
@@ -6,11 +6,11 @@ export default {
   exclude: [
     "eslint",
     "eslint-plugin-array-func", // needs flat config
-    // "eslint-plugin-vitest", // needs flat config
     "eslint-plugin-no-use-extend-native", // needs flat config
     "@stylistic/eslint-plugin-js", // needs eslint 9
     "@stylistic/eslint-plugin-jsx", // needs eslint 9
     "@stylistic/eslint-plugin-ts", // needs eslint 9
     "eslint-plugin-unicorn", // needs flat config and eslint 9
+    "eslint-plugin-storybook", // breaking changes in v9
   ],
 };

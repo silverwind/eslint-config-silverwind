5973d27017765b6b94e1f8f3c71dd57de0d8aa6c
silverwind <me@silverwind.io>
2025-09-16 12:39:26 +0200

    remove unnecessary ignores

--- index.ts
+++ index.ts
@@ -44,7 +44,6 @@ export default defineConfig([
   ]),
   {
     files: [`**/*.{${[...jsExts, ...tsExts].join(",")}}`],
-    ignores: ["dist/*"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",

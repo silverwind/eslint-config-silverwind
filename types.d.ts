380d99a6bbf94264612674357356a8eec115c534
silverwind <me@silverwind.io>
2025-09-23 08:50:43 +0200

    cleanup types

--- types.d.ts
+++ types.d.ts
@@ -3,16 +3,6 @@ declare module "@eslint-community/eslint-plugin-eslint-comments" {
   const plugin: Eslint.Plugin;
   export = plugin;
 }
-declare module "@stylistic/eslint-plugin-js" {
-  import type {Eslint} from "eslint";
-  const plugin: Eslint.Plugin;
-  export = plugin;
-}
-declare module "@stylistic/eslint-plugin-jsx" {
-  import type {Eslint} from "eslint";
-  const plugin: Eslint.Plugin;
-  export = plugin;
-}
 declare module "typescript-eslint" {
   import type {Eslint} from "eslint";
   const plugin: Eslint.Plugin;
@@ -24,11 +14,6 @@ declare module "eslint-plugin-array-func" {
   export = plugin;
 }
 declare module "eslint-plugin-sonarjs" {
-  import type {Eslint} from "eslint";
-  const plugin: Eslint.Plugin;
-  export = plugin;
-}
-declare module "eslint-plugin-import" {
   import type {Eslint} from "eslint";
   const plugin: Eslint.Plugin;
   export = plugin;
@@ -53,17 +38,17 @@ declare module "eslint-plugin-vitest" {
   const plugin: Eslint.Plugin;
   export = plugin;
 }
-declare module "eslint-plugin-vitest-globals" {
-  import type {Eslint} from "eslint";
-  const plugin: Eslint.Plugin;
-  export = plugin;
-}
 declare module "eslint-plugin-react" {
   import type {Eslint} from "eslint";
   const plugin: Eslint.Plugin;
   export = plugin;
 }
 declare module "eslint-plugin-react-hooks" {
+  import type {Eslint} from "eslint";
+  const plugin: Eslint.Plugin;
+  export = plugin;
+}
+declare module "eslint-plugin-jsx-a11y-x" {
   import type {Eslint} from "eslint";
   const plugin: Eslint.Plugin;
   export = plugin;
@@ -78,11 +63,4 @@ declare module "eslint-plugin-validate-jsx-nesting" {
   const plugin: Eslint.Plugin;
   export = plugin;
 }
-declare module "eslint-config-silverwind-react" {
-  const config: Record<string, any>;
-  export = config;
-}
-declare module "eslint-config-silverwind-typescript" {
-  const config: Record<string, any>;
-  export = config;
-}
+

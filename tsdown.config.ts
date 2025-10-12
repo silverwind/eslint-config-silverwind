0ab36c182b6f9157578147f88155f27e82bb59c4
silverwind <me@silverwind.io>
2025-10-12 08:41:22 +0200

    enable minify

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,4 +4,5 @@ import {defineConfig} from "tsdown";
 export default defineConfig(nodeLib({
   url: import.meta.url,
   shims: true, // needed for __dirname and __filename in use by some plugins
+  minify: true,
 }));

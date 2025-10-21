a2ba49f195adf7a898ef4564aa8225aa6a83eeee
silverwind <me@silverwind.io>
2025-10-21 19:01:40 +0200

    migrate to new react plugins

--- tests/react.tsx
+++ tests/react.tsx
@@ -4,5 +4,5 @@ import {useRef} from "react";
 function Component() { // eslint-disable-line react-refresh/only-export-components,@typescript-eslint/no-unused-vars
   const ref = useRef(0);
   const value = ref.current; // eslint-disable-line react-hooks/refs
-  return <div>{value}</div>;
+  return <><div>{value}</div></>; // eslint-disable-line @eslint-react/no-useless-fragment,@eslint-react/jsx-shorthand-fragment
 }

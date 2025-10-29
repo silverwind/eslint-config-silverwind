d7b8f5810def920a7291b9938a5f61b861e50407
silverwind <me@silverwind.io>
2025-10-29 12:12:23 +0100

    add test

--- tests/react.tsx
+++ tests/react.tsx
@@ -3,6 +3,8 @@ import {Fragment, useRef} from "react";
 // @ts-expect-error
 function Component() { // eslint-disable-line react-refresh/only-export-components,@typescript-eslint/no-unused-vars
   const ref = useRef(0);
+  // @ts-expect-error
+  const _ref2 = useRef(null); // eslint-disable-line no-restricted-syntax
   const value = ref.current; // eslint-disable-line react-hooks/refs
   // @ts-expect-error
   const _x = (

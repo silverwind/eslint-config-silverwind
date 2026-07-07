06134fbc8b489ac004b2487072a5c2d1a6675393
silverwind <me@silverwind.io>
2026-07-07 17:47:20 +0200

    update deps
    Remove now-dead unicorn/no-duplicate-if-branches disable in test fixture.

--- tests/test.tsx
+++ tests/test.tsx
@@ -16,7 +16,7 @@ type Baz = Bar & {baz?: string}; // eslint-disable-line @typescript-eslint/no-un
 let a = 0;
 if (foo) { // eslint-disable-line sonarjs/no-all-duplicated-branches
   a = 1;
-} else { // eslint-disable-line unicorn/no-duplicate-if-branches
+} else {
   a = 1; // eslint-disable-line @typescript-eslint/no-unused-vars
 }
 
